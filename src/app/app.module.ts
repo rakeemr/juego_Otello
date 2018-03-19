b93ac78cfb017420669a0a3f9ced6002d1faec3f
Stewart <stwartf64@gmail.com>
2018-03-18 22:35:47 -0600

    [Update Tablero]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 import { AppComponent } from './app.component';
@@ -10,8 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+      MDBBootstrapModule.forRoot(),
     BrowserModule
   ],
+    schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
